6ec0c235efc71165da83b6e74521214012d20e4e
R2H <r2h@AdminisatorsMBP.attlocal.net>
2022-09-26 22:01:56 -0400

    Medium Challenge

---
+++ Medium/medium.js
@@ -0,0 +1,31 @@
+//Medium challenge 
+
+let monthNum = prompt("Please enter a number 1-12");
+
+if (monthNum == 1){
+    console.log("January");
+} else if (monthNum == 2){
+    console.log("Febuary");
+}else if (monthNum == 3){
+    console.log("April");
+}else if (monthNum == 4){
+    console.log ("March");
+}else if (monthNum == 5){
+    console.log ("May");
+}else if (monthNum == 6){
+    console.log ("June");
+}else if (monthNum == 7){
+    console.log ("July");
+}else if (monthNum == 8){
+    console.log ("August");
+}else if (monthNum == 9){
+    console.log ("September");
+}else if (monthNum == 10){
+    console.log ("October");
+}else if (monthNum == 11){
+    console.log ("November");
+}else if (monthNum == 12){
+    console.log ("December");
+}else{
+   alert("Enter a correct number, please!");
+}
