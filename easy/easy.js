f5d0d2a99063e3cc7025f1745f550a72d9a9653b
R2H <r2h@AdminisatorsMBP.attlocal.net>
2022-09-26 21:58:55 -0400

    Easy Challenge

---
+++ easy/easy.js
@@ -0,0 +1,6 @@
+//Easy Challenge
+const fNames = ["Aida","Jazmine","Natalia"];
+const lNames = ["Arias","Rangel","Lozano"];
+let age= [23,24,25];
+
+console.log(`Hello, my name is ${fNames[1]} ${lNames[1]} and I am ${age[1]} years old.`);
